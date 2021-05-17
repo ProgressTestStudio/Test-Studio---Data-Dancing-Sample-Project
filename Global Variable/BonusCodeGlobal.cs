643003bb3bcae166191b0dff03f7e76210259c4e
kobolda <kobolda@users.noreply.github.com>
2021-05-17 19:37:56 +0300

    global variable creation

---
+++ Global Variable/BonusCodeGlobal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace new_test_project
+{
+    public class BonusCodeGlobal
+    {
+        
+        // declare public static var
+        public static string myBonusCode; 
+        
+    }
+}
