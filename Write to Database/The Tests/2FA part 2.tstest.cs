e49dd02e218b0dfbc06e3316683dd602eb8104f6
kobolda <kobolda@users.noreply.github.com>
2021-05-17 19:40:52 +0300

    Dynamic Database Binding

---
+++ Write to Database/The Tests/2FA part 2.tstest.cs
@@ -0,0 +1,71 @@
+using Telerik.TestStudio.Translators.Common;
+using Telerik.TestingFramework.Controls.TelerikUI.Blazor;
+using Telerik.TestingFramework.Controls.KendoUI.Angular;
+using Telerik.TestingFramework.Controls.KendoUI;
+using Telerik.WebAii.Controls.Html;
+using Telerik.WebAii.Controls.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+using ArtOfTest.Common.UnitTesting;
+using ArtOfTest.WebAii.Core;
+using ArtOfTest.WebAii.Controls.HtmlControls;
+using ArtOfTest.WebAii.Controls.HtmlControls.HtmlAsserts;
+using ArtOfTest.WebAii.Design;
+using ArtOfTest.WebAii.Design.Execution;
+using ArtOfTest.WebAii.ObjectModel;
+using ArtOfTest.WebAii.Silverlight;
+using ArtOfTest.WebAii.Silverlight.UI;
+
+namespace new_test_project
+{
+
+    public class _FA_as_if : BaseWebAiiTest
+    {
+        #region [ Dynamic Pages Reference ]
+
+        private Pages _pages;
+
+        /// <summary>
+        /// Gets the Pages object that has references
+        /// to all the elements, frames or regions
+        /// in this project.
+        /// </summary>
+        public Pages Pages
+        {
+            get
+            {
+                if (_pages == null)
+                {
+                    _pages = new Pages(Manager.Current);
+                }
+                return _pages;
+            }
+        }
+
+        #endregion
+        
+        // Add your test methods here...
+    
+        [CodedStep(@"New Coded Step")]
+        public void _2FA_as_if_CodedStep()
+        {
+            
+            
+             string originalFile = "C:\\your directory, by default it is the Test Studio Data folder in My Documents which is populated after you databind a test\\2FA.xlsx";  
+            
+            
+             string fileToReplace  = "C:\\_Progress\\2FA.xlsx";  
+             string backUpOfFileToReplace = "C:\\_Progress\\2FA_backup.xlsx";
+            
+          
+            File.Delete(originalFile);
+            File.Copy(fileToReplace, originalFile);  
+            
+            
+        }
+    }
+}
